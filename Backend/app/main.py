305f190386383d8d7c616f06a68fa52c62b02b68
Cipher-Shadow-IR <202154115+Cipher-Shadow-IR@users.noreply.github.com>
2025-12-27 11:42:55 +0530

    fix: routers

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.models import *
 
-from app.routers import equipment, teams, maintenance_requests
+from app.routers import equipment, teams, maintenance_request
 
 Base.metadata.create_all(bind=engine)
 
@@ -11,8 +11,8 @@ app = FastAPI(title="FixTrack API")
 app.include_router(equipment.router, prefix="/api/equipment", tags=["Equipment"])
 app.include_router(teams.router, prefix="/api/maintenance-teams", tags=["Teams"])
 app.include_router(
-    maintenance_requests.router,
-    prefix="/api/maintenance-request",
+    maintenance_request.router,
+    prefix="/api/maintenance_request",
     tags=["Maintenance Requests"]
 )
 
